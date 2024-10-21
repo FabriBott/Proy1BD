a8896af577644fa258e28edf4de197c69d837158
FabriBott <fabripicado@gmail.com>
2024-10-20 18:29:20 -0600

    FixRedis2.0

--- DB/redis.py
+++ DB/redis.py
@@ -1,32 +1,30 @@
 import os
 import redis
 
-class RedisClient:
-    def init_redis(self):
-        self.redis_client = None
-        self._connect()
+# Inicializar Redis
+def init_redis():
+    try:
+        # Cargar configuraciones de entorno con valores por defecto
+        host = os.getenv("REDIS_HOST", "redis")
+        port = int(os.getenv("REDIS_PORT", 6379))
+        db = int(os.getenv("REDIS_DB", 0))  # Puedes usar varios DBs en Redis
 
-    def _connect(self):
-        try:
-            # Cargar configuraciones de entorno con valores por defecto
-            host = os.getenv("REDIS_HOST", "redis")
-            port = int(os.getenv("REDIS_PORT", 6379))
-            db = int(os.getenv("REDIS_DB", 0))  # Puedes usar varios DBs en Redis
-            self.redis_client = redis.StrictRedis(
-                host=host,
-                port=port,
-                db=db,
-                decode_responses=True,
-                socket_timeout=5  # Tiempo máximo de espera para conectar
-            )
-            self.redis_client.ping()  # Verificar si la conexión está activa
-            print(f"Conectado exitosamente a Redis en {host}:{port}")
-        except redis.ConnectionError as e:
-            print(f"Error de conexión a Redis: {e}")
-        except Exception as e:
-            print(f"Error inesperado al conectar a Redis: {e}")
+        # Crear el cliente de Redis
+        redis_client = redis.StrictRedis(
+            host=host,
+            port=port,
+            db=db,
+            decode_responses=True,
+            socket_timeout=5  # Tiempo máximo de espera para conectar
+        )
+        
+        # Verificar si la conexión está activa
+        redis_client.ping()  
+        print(f"Conectado exitosamente a Redis en {host}:{port}")
+        return redis_client
+    except redis.ConnectionError as e:
+        print(f"Error de conexión a Redis: {e}")
+    except Exception as e:
+        print(f"Error inesperado al conectar a Redis: {e}")
+        return None
 
-    def get_client(self):
-        if not self.redis_client:
-            self._connect()
-        return self.redis_client
