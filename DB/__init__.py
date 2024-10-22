c0725d97077360447184687aea2d2814c5ac6ca0
Charlie00CR <113566517+Charlie00CR@users.noreply.github.com>
2024-10-22 10:33:56 -0600

    Update __init__.py

--- DB/__init__.py
+++ DB/__init__.py
@@ -1,6 +1,6 @@
-from .postgres import init_postgres
-from .redis import init_redis
-from .mongo import init_mongo
+from postgres import init_postgres
+from redis import init_redis
+from mongo import init_mongo
 
 # Inicializar todas las bases de datos
 def init_databases():
