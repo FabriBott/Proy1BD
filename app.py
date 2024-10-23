70a51106eadf27dd0e92bd2b62ad67c9522d6436
FabriBott <fabripicado@gmail.com>
2024-10-22 18:28:59 -0600

    RedisFixd

--- app.py
+++ app.py
@@ -84,11 +84,11 @@ async def startup():
 @app.post("/usuarios/")
 async def crear_usuario_endpoint(usuario: UsuarioCreate):
     
-    # Verificar si el usuario ya está en caché
-    cached_user = redis_client.get(f"user:{usuario.username}")
-    if cached_user:
-        logger.info(f"Usuario {usuario.username} recuperado de la caché.")
-        return json.loads(cached_user)  # Convertir de JSON a dict
+    # # Verificar si el usuario ya está en caché
+    # cached_user = redis_client.get(f"user:{usuario.username}")
+    # if cached_user:
+    #     logger.info(f"Usuario {usuario.username} recuperado de la caché.")
+    #     return json.loads(cached_user)  # Convertir de JSON a dict
 
     nuevo_usuario = crear_usuario(
         nombre=usuario.nombre,
@@ -98,9 +98,9 @@ async def crear_usuario_endpoint(usuario: UsuarioCreate):
         fechaRegistro=usuario.fechaRegistro
     )
 
-    # Guardar el nuevo usuario en caché
-    redis_client.set(f"user:{usuario.username}", json.dumps(nuevo_usuario))
-    logger.info(f"Usuario {usuario.username} almacenado en caché.")
+    # # Guardar el nuevo usuario en caché
+    # redis_client.set(f"user:{usuario.username}", json.dumps(nuevo_usuario))
+    # logger.info(f"Usuario {usuario.username} almacenado en caché.")
 
     return nuevo_usuario
 
@@ -182,11 +182,11 @@ def get_admin_token() -> adminToken:
 def create_user(user: NewUser):
     global tokenAdministrativo  # Usar la variable global
 
-    # Verificar si el usuario ya está en caché
-    cached_user = redis_client.get(f"user:{user.username}")
-    if cached_user:
-        logger.info(f"Usuario {user.username} recuperado de la caché.")
-        return json.loads(cached_user)
+    # # Verificar si el usuario ya está en caché
+    # cached_user = redis_client.get(f"user:{user.username}")
+    # if cached_user:
+    #     logger.info(f"Usuario {user.username} recuperado de la caché.")
+    #     return json.loads(cached_user)
 
     # Verificar si el token administrativo está disponible, si no, llamarlo
     if tokenAdministrativo is None:
@@ -223,10 +223,8 @@ def create_user(user: NewUser):
     # Realizar la solicitud POST para crear el usuario
     response = requests.post(url, json=data, headers=headers)
 
-    redis_client.set(f"user:{user.username}", json.dumps(data))  # Ajustar según el formato del usuario creado
-    logger.info(f"Usuario {user.username} almacenado en caché.")
-
-    # return {"message": "Usuario creado exitosamente"}
+    # redis_client.set(f"user:{user.username}", json.dumps(data))  # Ajustar según el formato del usuario creado
+    # logger.info(f"Usuario {user.username} almacenado en caché.")
 
     # Verificar la respuesta
     if response.status_code == 201:
